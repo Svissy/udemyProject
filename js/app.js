c41c62dddb2f6b6520cbd4223e448bea70f7405b
Dmitriy <megoepiccat@gmail.com>
2021-07-27 11:41:00 +0300

    1st task

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 "use strict";
 
-const isChecked = true;
-const isClosed = false;
+let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-console.log(isChecked || isClosed);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
